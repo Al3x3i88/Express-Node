1e95ea7627d2fa944e32057d287137585715253b
Alexei Ramirez Duran <alexeirmrz3@gmail.com>
2023-01-28 17:54:37 -0500

    Trabajando con rutas

--- index.js
+++ index.js
@@ -6,11 +6,23 @@ var app = express();
 
 // app.use('/', express.static('static'))
 app.get('/', (req, res)=>{
-    res.sendFile('./static/index.html', {
-        root: __dirname
-    })
+    // res.sendFile('./static/index.html', {
+    //     root: __dirname
+    // })
+    res.send('Hello World')
 })
 
+app.get('/about', (req, res)=>{
+   res.send('Acerca de...')
+})
+
+app.get('/clima', (req, res)=>{
+    res.send('El clima esta hermoso')
+})
+
+app.use((req, res)=>{
+    res.status(404).send('No se encontro tu pagina')
+})
 app.listen(PORT, ()=>{
 
     console.log(`Server on port ${PORT}`);
