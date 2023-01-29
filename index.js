23ee7a7616b41f0b60c0c2c9f50f4e4657af158f
Alexei Ramirez Duran <alexeirmrz3@gmail.com>
2023-01-28 21:34:56 -0500

    Usando los metodos http

--- index.js
+++ index.js
@@ -2,22 +2,26 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
-// app.use('/', express.static('static'))
-app.get('/', (req, res)=>{
-    // res.sendFile('./static/index.html', {
-    //     root: __dirname
-    // })
-    res.send('Hello World')
+app.get('/products', (req, res)=>{
+    res.send('Lista de productos')
 })
 
-app.get('/about', (req, res)=>{
-   res.send('Acerca de...')
+app.post('/products', (req, res)=>{
+    res.send('Creando Productos')
 })
 
-app.get('/clima', (req, res)=>{
-    res.send('El clima esta hermoso')
+app.put('/products', (req, res)=>{
+    res.send('Actualizando productos')
+})
+
+app.delete('/products', (req, res)=>{
+    res.send('Eliminando un producto')
+})
+
+app.patch('/products', (req, res)=>{
+    res.send('Actualizando una parte de productos')
 })
 
 app.use((req, res)=>{
