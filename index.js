5da8017d091a7eb7931dafbfa9882579b47bfd22
Alexei Ramirez Duran <alexeirmrz3@gmail.com>
2023-01-28 23:07:52 -0500

    enviando datos desde el cliente

--- index.js
+++ index.js
@@ -4,25 +4,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/products', (req, res)=>{
-    res.send('Lista de productos')
-})
+app.use(express.text())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.post('/products', (req, res)=>{
-    res.send('Creando Productos')
-})
-
-app.put('/products', (req, res)=>{
-    res.send('Actualizando productos')
-})
-
-app.delete('/products', (req, res)=>{
-    res.send('Eliminando un producto')
-})
-
-app.patch('/products', (req, res)=>{
-    res.send('Actualizando una parte de productos')
-})
+app.post('/user', (req, res)=>{
+    console.log(req.body);
+    res.send(' Nuevo usuario creado')
+}) 
 
 app.use((req, res)=>{
     res.status(404).send('No se encontro tu pagina')
